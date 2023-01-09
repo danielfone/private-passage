160a8a3f2e4e3b4ce0fa1ec6ca878c5da34a220a
Daniel Fone <daniel@fone.net.nz>
2023-01-09 14:42:29 +1300

    Tidy relay server

--- relay_server.rb
+++ relay_server.rb
@@ -32,11 +32,11 @@ class RelayServer
 private
 
   def initialize_connection(env)
-    ws = Faye::WebSocket.new(env, nil, { ping: KEEPALIVE_TIME })
-    client_id = ws.object_id
+    websocket = Faye::WebSocket.new(env, nil, { ping: KEEPALIVE_TIME })
+    client_id = websocket.object_id
 
     # Log the origin of the connection
-    log("New connection", origin: ws.env['HTTP_ORIGIN'], client_id:)
+    log("New connection", origin: websocket.env['HTTP_ORIGIN'], client_id:)
 
     # Parse the client ID from the query string
     query = Rack::Utils.parse_query(env['QUERY_STRING'])
@@ -47,72 +47,72 @@ private
 
     # Close the connection if the channel ID is missing
     if channel_id.nil? || channel_id.empty?
-      ws.close(4000, 'Channel ID is required')
+      websocket.close(4000, 'Channel ID is required')
       log("Invalid connection, missing channel_id", client_id: client_id)
-      return ws.rack_response
+      return websocket.rack_response
     end
 
-    ws.on :open do |event|
+    websocket.on :open do
       if existing
-        add_client(channel_id, ws)
+        add_client(channel_id, websocket)
       else
-        create_channel(channel_id, ws)
+        create_channel(channel_id, websocket)
       end
     end
 
-    ws.on :message do |event|
-      relay_message(channel_id, ws, event.data)
+    websocket.on :message do |event|
+      relay_message(channel_id, websocket, event.data)
     end
 
-    ws.on :close do |event|
-      remove_client(channel_id, ws)
-      ws = nil # Allow ws to be garbage collected
+    websocket.on :close do
+      remove_client(channel_id, websocket)
+      websocket = nil # Allow websocket to be garbage collected
     end
 
     # Return async Rack response
-    ws.rack_response
+    websocket.rack_response
   end
 
   # Create a new channel if it doesn't exist adn add the client to it
-  def create_channel(channel_id, ws)
+  def create_channel(channel_id, websocket)
     if @channels.key?(channel_id)
-      ws.close(4000, 'Channel already open')
-      log("Channel already open", channel_id:, client_id: ws.object_id)
+      websocket.close(4000, 'Channel already open')
+      log("Channel already open", channel_id:, client_id: websocket.object_id)
       return
     end
 
     @channels[channel_id] = Set.new
-    @channels[channel_id] << ws
-    log("Opened channel", channel_id:, client_id: ws.object_id)
+    @channels[channel_id] << websocket
+    log("Opened channel", channel_id:, client_id: websocket.object_id)
   end
 
   # Add a client to an existing channel
-  def add_client(channel_id, ws)
+  def add_client(channel_id, websocket)
     if !@channels.key?(channel_id)
-      ws.close(4000, 'Channel not found')
-      log("Channel not found", channel_id:, client_id: ws.object_id)
+      websocket.close(4000, 'Channel not found')
+      log("Channel not found", channel_id:, client_id: websocket.object_id)
       return
     end
 
-    @channels[channel_id] << ws
-    log("Connected to channel", channel_id:, client_id: ws.object_id)
+    @channels[channel_id] << websocket
+    log("Connected to channel", channel_id:, client_id: websocket.object_id)
   end
 
   # Relay a message to all clients in a channel except the sender
-  def relay_message(channel_id, ws, message)
+  def relay_message(channel_id, websocket, message)
     @channels[channel_id].each do |peer|
-      next if peer == ws
+      next if peer == websocket
       peer.send(message)
     end
   end
 
   # Remove a client from a channel and close the channel if it's empty
-  def remove_client(channel_id, ws)
+  def remove_client(channel_id, websocket)
     channel = @channels[channel_id]
     return unless channel
 
-    channel.delete(ws)
-    log("Closed connection", channel_id:, client_id: ws.object_id)
+    channel.delete(websocket)
+    log("Closed connection", channel_id:, client_id: websocket.object_id)
 
     if channel.empty?
       @channels.delete(channel_id)
